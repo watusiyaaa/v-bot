1e339261a9ba5be8cde896dfe91a1646abc715ba
watusiyaaa <sandiganjobelle13@gmail.com>
2025-02-12 00:16:44 +0800

    new file:   src/commands/cmdregister.js

---
+++ src/commands/cmdregister.js
@@ -0,0 +1,43 @@
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const { Client, GatewayIntentBits } = require('discord.js');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = function registerCommands(client) {
+    const commands = [];
+    const commandsPath = path.join(__dirname, 'commands');
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const command = require(path.join(commandsPath, file));
+        commands.push(command.data.toJSON());
+    }
+
+    const rest = new REST({ version: '10' }).setToken(process.env.V_BOT_TOKEN);
+
+    (async () => {
+        try {
+            console.log('Started refreshing application (/) commands.');
+
+            await rest.put(
+                Routes.applicationGuildCommands(client.user.id, process.env.GUILD_ID),
+                { body: commands },
+            );
+
+            console.log('Successfully reloaded application (/) commands.');
+        } catch (error) {
+            console.error(error);
+        }
+    })();
+};
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once('ready', () => {
+    console.log('Now online!');
+});
+
+client.login(process.env.V_BOT_TOKEN);
