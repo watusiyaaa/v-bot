d7792e162efea958b3e8cf78cdde43e31d5a32c4
watusiyaaa <sandiganjobelle13@gmail.com>
2025-02-12 18:14:39 +0800

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ client.once(Events.ClientReady, async (c) => {
 // just in case someone asks for the requirements again
 client.on('messageCreate', async (msg) => {
     if(msg.content === 'reqs'){
-        msg.reply('- You play Rhythm Hive. Provide a picture of your Rhythm Hive profile (as shown in the 1st picture below)\n- You must be a member of **RHCord** ([**press here to join the server**](https://discord.gg/MQdvHVpjca)), and your current level role there must STRICTLY be **A Class** and above (as also shown in the 2nd picture below, type **__t!rank__** to get this pic from the server)');
+        msg.reply('# Verification Requirements\n- You play Rhythm Hive. Provide a picture of your Rhythm Hive profile (as shown in the 1st picture below)\n- You must be a member of **RHCord** ([**press here to join the server**](https://discord.gg/MQdvHVpjca)), and your current level role there must STRICTLY be **A Class** and above (as also shown in the 2nd picture below, type **__t!rank__** to get this pic from the server)');
     }
 })
 
