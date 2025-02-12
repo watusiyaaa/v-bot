9cd6c179307f78d23463fc1010dc60223fe4175d
watusiyaaa <sandiganjobelle13@gmail.com>
2025-02-12 12:15:29 +0800

    new file:   src/botstatus.js

---
+++ src/botstatus.js
@@ -0,0 +1,21 @@
+// having your bot have a status
+//another activites: PLAYING, WATCHING, STREAMING, COMPETING
+
+const { Events, ActivityType } = require('discord.js'); // Import necessary classes
+
+module.exports = (client) => {
+    client.once(Events.ClientReady, async (c) => {
+        console.log(`Status in!`);
+
+        // Set the bot's presence
+        client.user.setPresence({
+            status: 'online',
+            activities: [
+                {
+                    name: 'FRI(END)S | 24/7)',
+                    type: ActivityType.Listening, // Set activity type
+                },
+            ],
+        });
+    });
+};
