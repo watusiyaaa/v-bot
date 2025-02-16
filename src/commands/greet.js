8fe07fc0dc12be6136889a20237c8b8a6c44d875
watusiyaaa <sandiganjobelle13@gmail.com>
2025-02-16 18:43:02 +0800

    modified:   src/commands/greet.js

--- src/commands/greet.js
+++ src/commands/greet.js
@@ -1,62 +1,70 @@
-const { EmbedBuilder, SlashCommandBuilder, PermissionsBitField } = require('discord.js');
+// testing greet embed
+const { EmbedBuilder, SlashCommandBuilder, PermissionsBitField, MessageFlags } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('greet')
-        .setDescription('Sends a greeting message'),
+        .setDescription('greet embed test'),
     name: 'greet',
-    description: 'Sends a greeting message',
-    async execute(message, args) {
-        const user = message.author;
-        const member = message.guild.members.cache.get(user.id);
+    description: 'greet embed test',
+    async execute(pmsg) {
+        const user = pmsg.author;
+        const member = pmsg.guild.members.cache.get(user.id);
 
 
         if (!member.permissions.has(PermissionsBitField.Flags.Administrator)) {
-            return message.reply({ content: 'You are not an admin!' });
+            return pmsg.reply({
+                content: 'You\'re not an admin!',
+                flags: [MessageFlags.Ephemeral] });
         }
 
-        // change the image URL to the image or gif you want (actually feel free to edit this whole embed lol just make sure you do the same in the actual greet embed)
-        const embed = new EmbedBuilder()
+        // test greet embed
+        const tembed = new EmbedBuilder()
             .setColor('#CDF7F6')
-            .setAuthor({ 
+            .setAuthor({
                 name: '✅ Verification Successful!',
-                iconURL: message.guild?.iconURL({ size: 512 }) ?? '' 
+                iconURL: pmsg.guild?.iconURL({ size: 512 }) ?? ''
             })
-
             .setTitle('**ᴄᴏᴢʏ ʀʜ ᴄᴀꜰᴇ 🍓 server**')
-            .setDescription(`Welcome ${user}!\n\nMake sure to read the <#1335169467424178266> and get your <#1335169556594823198> in these respective channels.\nHope you enjoy your stay in the server!`)
-            .setThumbnail(user.avatarURL({ extension: 'png', size: 512 }))
+            .setDescription(`**Welcome to the server ${user}!**
+                \n\nMake sure to read the <#1335169467424178266> and get your <#1335169556594823198> in these respective channels.
+                \nHope you enjoy your stay in the server!`)
+                .setThumbnail(user.avatarURL({ extension: 'png', size: 512 }) ?? 'https://media.discordapp.net/attachments/1335172660182646927/1340632959467651122/discordgrey.png?ex=67b3113d&is=67b1bfbd&hm=e0e9ca814279a29237790ddb721a70821a470cb8256d81984fb9fd833d82182f&=&format=webp&quality=lossless&width=320&height=320')
             .setImage('https://media.discordapp.net/attachments/1335172660182646927/1339065138589732915/greet_gif.gif?ex=67ad5d17&is=67ac0b97&hm=cd7e5417333194473cfb73a1eb087f2016d6867a5253241d3981c1b04982e936&=&width=622&height=346')
-            .setFooter({ text: `You're our ${message.guild ? getOrdinalSuffix(message.guild.memberCount) : 'unknown'} member!` })
-
+            .setFooter({text: `You're our ${pmsg.guild ? getOrdinalSuffix(pmsg.guild.memberCount): 'unknown'} member!`})
             .setTimestamp(new Date());
-
-        await message.channel.send({ embeds: [embed] });
+                
+        await pmsg.channel.send({embeds: [tembed]});
 
     },
     
-    async executeSlash(interaction) {
-        const user = interaction.user;
-        const member = interaction.guild.members.cache.get(user.id);
+    async executeSlash(smsg) {
+        const user = smsg.user;
+        const member = smsg.guild.members.cache.get(user.id);
 
         if (!member.permissions.has(PermissionsBitField.Flags.Administrator)) {
-            return interaction.reply({ content: 'You are not an admin!', ephemeral: true });
+            return smsg.reply({
+                content: 'You\'re not an admin!',
+                flags: [MessageFlags.Ephemeral] });
         }
 
-        const embed = new EmbedBuilder()
+        // test greet embed
+        const tembed = new EmbedBuilder()
             .setColor('#CDF7F6')
-            .setAuthor({ 
+            .setAuthor({
                 name: '✅ Verification Successful!',
-                iconURL: interaction.guild?.iconURL({ size: 512 }) ?? '' 
+                iconURL: smsg.guild?.iconURL({ size: 512 }) ?? ''
             })
             .setTitle('**ᴄᴏᴢʏ ʀʜ ᴄᴀꜰᴇ 🍓 server**')
-            .setDescription(`Welcome ${user}!\n\nMake sure to read the <#1335169467424178266> and get your <#1335169556594823198> in these respective channels.\nHope you enjoy your stay in the server!`)
-            .setThumbnail(user.avatarURL({ extension: 'png', size: 512 }))
+            .setDescription(`**Welcome to the server ${user}!**
+                \n\nMake sure to read the <#1335169467424178266> and get your <#1335169556594823198> in these respective channels.
+                \nHope you enjoy your stay in the server!`)
+            .setThumbnail(user.avatarURL({ extension: 'png', size: 512 }) ?? 'https://media.discordapp.net/attachments/1335172660182646927/1340632959467651122/discordgrey.png?ex=67b3113d&is=67b1bfbd&hm=e0e9ca814279a29237790ddb721a70821a470cb8256d81984fb9fd833d82182f&=&format=webp&quality=lossless&width=320&height=320')
             .setImage('https://media.discordapp.net/attachments/1335172660182646927/1339065138589732915/greet_gif.gif?ex=67ad5d17&is=67ac0b97&hm=cd7e5417333194473cfb73a1eb087f2016d6867a5253241d3981c1b04982e936&=&width=622&height=346')
-            .setFooter({ text: `You're our ${interaction.guild ? getOrdinalSuffix(interaction.guild.memberCount) : 'unknown'} member!` })
+            .setFooter({text: `You're our ${smsg.guild ? getOrdinalSuffix(smsg.guild.memberCount): 'unknown'} member!`})
             .setTimestamp(new Date());
-
-        await interaction.reply({ embeds: [embed] });
+                
+        await smsg.channel.send({embeds: [tembed]});
     }
 
 };
