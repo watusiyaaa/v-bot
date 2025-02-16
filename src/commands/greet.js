a7bc71a2731543ca8f6054039e485f7919ab43c3
watusiyaaa <sandiganjobelle13@gmail.com>
2025-02-16 11:25:16 +0800

    modified:   src/commands/greet.js

--- src/commands/greet.js
+++ src/commands/greet.js
@@ -12,7 +12,7 @@ module.exports = {
 
 
         if (!member.permissions.has(PermissionsBitField.Flags.Administrator)) {
-            return interactionOrMessage.reply({ content: 'You are not an admin!', ephemeral: true });
+            return message.reply({ content: 'You are not an admin!' });
         }
 
         // change the image URL to the image or gif you want (actually feel free to edit this whole embed lol just make sure you do the same in the actual greet embed)
@@ -20,16 +20,18 @@ module.exports = {
             .setColor('#CDF7F6')
             .setAuthor({ 
                 name: '✅ Verification Successful!',
-                iconURL: interactionOrMessage.guild?.iconURL({ size: 512 }) ?? '' 
+                iconURL: message.guild?.iconURL({ size: 512 }) ?? '' 
             })
+
             .setTitle('**ᴄᴏᴢʏ ʀʜ ᴄᴀꜰᴇ 🍓 server**')
             .setDescription(`Welcome ${user}!\n\nMake sure to read the <#1335169467424178266> and get your <#1335169556594823198> in these respective channels.\nHope you enjoy your stay in the server!`)
             .setThumbnail(user.avatarURL({ extension: 'png', size: 512 }))
             .setImage('https://media.discordapp.net/attachments/1335172660182646927/1339065138589732915/greet_gif.gif?ex=67ad5d17&is=67ac0b97&hm=cd7e5417333194473cfb73a1eb087f2016d6867a5253241d3981c1b04982e936&=&width=622&height=346')
-            .setFooter({ text: `You're our ${interactionOrMessage.guild ? getOrdinalSuffix(interactionOrMessage.guild.memberCount) : 'unknown'} member!` })
+            .setFooter({ text: `You're our ${message.guild ? getOrdinalSuffix(message.guild.memberCount) : 'unknown'} member!` })
+
             .setTimestamp(new Date());
 
-        await message.reply({ embeds: [embed] });
+        await message.channel.send({ embeds: [embed] });
 
     },
     
