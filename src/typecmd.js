7385b387948d76dc447b6693df8fe11253665c72
watusiyaaa <sandiganjobelle13@gmail.com>
2025-02-16 13:20:45 +0800

    modified:   src/typecmd.js

--- src/typecmd.js
+++ src/typecmd.js
@@ -14,7 +14,7 @@ module.exports = {
       }
 
       msg.channel.send({
-        content: '# Verification Requirements\n- You play Rhythm Hive. Provide a picture of your Rhythm Hive profile (as shown in the 1st picture below)\n- You must be a member of **RHCord** ([**press here to join the server**](https://discord.gg/MQdvHVpjca)), and your current level role there must STRICTLY be **A Class** and above (as also shown in the 2nd picture below, type **__t!rank__** to get this pic from the server)',
+        content: '# Verification Requirements\n- You play Rhythm Hive. Provide a picture of your Rhythm Hive profile (as shown in the 1st picture below)\n- You must be a member of **RHCord** ([**press here to join the server**]<https://discord.gg/MQdvHVpjca>), and your current level role there must STRICTLY be **A Class** and above (as also shown in the 2nd picture below, type **__t!rank__** to get this pic from the server)',
         files: [imagePath],
 
         allowedMentions: { repliedUser: false }
