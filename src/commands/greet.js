b83bd0c2d20d6ba15d0f0f3bb56647ee0e9c50c8
watusiyaaa <sandiganjobelle13@gmail.com>
2025-02-18 20:46:44 +0800

    modified:   src/commands/greet.js 	modified:   src/index.js

--- src/commands/greet.js
+++ src/commands/greet.js
@@ -29,7 +29,7 @@ module.exports = {
             .setDescription(`**Welcome to the server ${user}!**
                 \n\nMake sure to read the <#1335169467424178266> and get your <#1335169556594823198> in these respective channels.
                 \nHope you enjoy your stay in the server!`)
-            .setThumbnail(user.avatarURL({ extension: 'png', size: 512 }) ?? 'https://media.discordapp.net/attachments/1335172660182646927/1340632959467651122/discordgrey.png?ex=67b3113d&is=67b1bfbd&hm=e0e9ca814279a29237790ddb721a70821a470cb8256d81984fb9fd833d82182f&=&format=webp&quality=lossless&width=320&height=320')
+            .setThumbnail(user.avatarURL({ extension: 'png', size: 512 }))
             .setImage('https://media.discordapp.net/attachments/1335172660182646927/1339065138589732915/greet_gif.gif?ex=67ad5d17&is=67ac0b97&hm=cd7e5417333194473cfb73a1eb087f2016d6867a5253241d3981c1b04982e936&=&width=622&height=346')
             .setFooter({text: `You're our ${pmsg.guild ? getOrdinalSuffix(pmsg.guild.memberCount): 'unknown'} member!`})
             .setTimestamp(new Date());
@@ -38,12 +38,12 @@ module.exports = {
 
     },
     
-    async executeSlash(smsg) {
-        const user = smsg.user;
-        const member = smsg.guild.members.cache.get(user.id);
+    async executeSlash(interaction) {
+        const user = interaction.user;
+        const member = interaction.guild.members.cache.get(user.id);
 
         if (!member.permissions.has(PermissionsBitField.Flags.Administrator)) {
-            return smsg.reply({
+            return interaction.reply({
                 content: 'You\'re not an admin!',
                 flags: [MessageFlags.Ephemeral] });
         }
@@ -53,18 +53,18 @@ module.exports = {
             .setColor('#CDF7F6')
             .setAuthor({
                 name: '✅ Verification Successful!',
-                iconURL: smsg.guild?.iconURL({ size: 512 }) ?? ''
+                iconURL: interaction.guild?.iconURL({ size: 512 }) ?? ''
             })
             .setTitle('**ᴄᴏᴢʏ ʀʜ ᴄᴀꜰᴇ 🍓 server**')
             .setDescription(`**Welcome to the server ${user}!**
                 \n\nMake sure to read the <#1335169467424178266> and get your <#1335169556594823198> in these respective channels.
                 \nHope you enjoy your stay in the server!`)
-            .setThumbnail(user.avatarURL({ extension: 'png', size: 512 }) ?? 'https://media.discordapp.net/attachments/1335172660182646927/1340632959467651122/discordgrey.png?ex=67b3113d&is=67b1bfbd&hm=e0e9ca814279a29237790ddb721a70821a470cb8256d81984fb9fd833d82182f&=&format=webp&quality=lossless&width=320&height=320')
+            .setThumbnail(user.avatarURL({ extension: 'png', size: 512 }))
             .setImage('https://media.discordapp.net/attachments/1335172660182646927/1339065138589732915/greet_gif.gif?ex=67ad5d17&is=67ac0b97&hm=cd7e5417333194473cfb73a1eb087f2016d6867a5253241d3981c1b04982e936&=&width=622&height=346')
-            .setFooter({text: `You're our ${smsg.guild ? getOrdinalSuffix(smsg.guild.memberCount): 'unknown'} member!`})
+            .setFooter({text: `You're our ${interaction.guild ? getOrdinalSuffix(interaction.guild.memberCount): 'unknown'} member!`})
             .setTimestamp(new Date());
                 
-        await smsg.channel.send({embeds: [tembed]});
+        await interaction.reply({embeds: [tembed]});
     }
 
 };
