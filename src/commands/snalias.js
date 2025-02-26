66bdc3eecd0c7bec117549513fda7a50b79b4a74
watusiyaaa <sandiganjobelle13@gmail.com>
2025-02-26 23:52:27 +0800

    modified:   src/commands/snalias.js 	modified:   src/commands/song-notes/snhandler.js

--- src/commands/snalias.js
+++ src/commands/snalias.js
@@ -1,47 +1,48 @@
 // alias
-const{ SlashCommandBuilder, EmbedBuilder, AllowedMentionsTypes } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, AllowedMentionsTypes } = require('discord.js');
 
-module.exports={
+module.exports = {
     data: new SlashCommandBuilder()
-    .setName('snalias')
-    .setDescription('Alias list for song notes commands'),
+        .setName('snalias')
+        .setDescription('Alias list for song notes commands'),
 
     name: 'snalias',
     description: 'Alias list for song notes commands',
 
     async executeSlash(interaction) {
         const alembed = new EmbedBuilder()
-        .setColor('#CDF7F6')
-        .setTitle('**Song notes alias**')
-        .setDescription('Song notes command structure: `.songnotes/.sn <group>`')
-        .addFields(
-            {name: '__BTS__', value: '`bangtan` or `bts`'},
-            {name: '__Tomorrow X Together__', value: '`tubatu` or `txt`'},
-            {name: '__Enhypen__', value: '`enhypen` or `enha`'},
-            {name: '__Seventeen__', value: '`seventeen` or `svt`'},
-            {name: '__Le Sserafim__', value: '`lesserafim` or `lsf`'},
-            {name: '__Newjeans__', value: '`newjeans` or `nj`'},
-            {name: '__Boynextdoor__', value: '`boynextdoor` or `bnd`'},
-            {name: '__Illit__', value: '`illit` or `ilt`'}
-        )
-        await interaction.reply({embeds: [alembed]});
+            .setColor('#CDF7F6')
+            .setTitle('**Song notes alias**')
+            .setDescription('Song notes command structure: `.songnotes/.sn <group>`\n-# Legend: <:nonrn:1344330008486740049> - **Non-renewed Song**')
+            .addFields(
+                { name: '__BTS__', value: '`bangtan` or `bts`' },
+                { name: '__Tomorrow X Together__', value: '`tubatu` or `txt`' },
+                { name: '__Enhypen__', value: '`enhypen` or `enha`' },
+                { name: '__Seventeen__', value: '`seventeen` or `svt`' },
+                { name: '__Le Sserafim__', value: '`lesserafim` or `lsf`' },
+                { name: '__Newjeans__', value: '`newjeans` or `nj`' },
+                { name: '__Boynextdoor__', value: '`boynextdoor` or `bnd`' },
+                { name: '__Illit__', value: '`illit` or `ilt`' }
+            );
+
+        await interaction.reply({ embeds: [alembed] });
     },
 
     async execute(alpr) {
         const alembed = new EmbedBuilder()
-        .setColor('#CDF7F6')
-        .setTitle('**Song notes alias**')
-        .setDescription('Song notes command structure: `.songnotes/.sn <group>`')
-        .addFields(
-            {name: '__BTS__', value: '`bangtan` or `bts`'},
-            {name: '__Tomorrow X Together__', value: '`tubatu` or `txt`'},
-            {name: '__Enhypen__', value: '`enhypen` or `enha`'},
-            {name: '__Seventeen__', value: '`seventeen` or `svt`'},
-            {name: '__Le Sserafim__', value: '`lesserafim` or `lsf`'},
-            {name: '__Newjeans__', value: '`newjeans` or `nj`'},
-            {name: '__Boynextdoor__', value: '`boynextdoor` or `bnd`'},
-            {name: '__Illit__', value: '`illit` or `ilt`'}
-        )
+            .setColor('#CDF7F6')
+            .setTitle('**Song notes alias**')
+            .setDescription('Song notes command structure: `.songnotes/.sn <group>`')
+            .addFields(
+                { name: '__BTS__', value: '`bangtan` or `bts`' },
+                { name: '__Tomorrow X Together__', value: '`tubatu` or `txt`' },
+                { name: '__Enhypen__', value: '`enhypen` or `enha`' },
+                { name: '__Seventeen__', value: '`seventeen` or `svt`' },
+                { name: '__Le Sserafim__', value: '`lesserafim` or `lsf`' },
+                { name: '__Newjeans__', value: '`newjeans` or `nj`' },
+                { name: '__Boynextdoor__', value: '`boynextdoor` or `bnd`' },
+                { name: '__Illit__', value: '`illit` or `ilt`' }
+            );
         await alpr.reply({
             embeds: [alembed],
             allowedMentions: { repliedUser: false }
